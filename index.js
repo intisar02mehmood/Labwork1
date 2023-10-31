387fedac792edd57d6fc3eaeab55a1a8f27ce82f
Intisar Mehmood <83180510+intisar02mehmood@users.noreply.github.com>
2023-10-31 11:19:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,76 @@
-// script.js
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
 
-// Image Slider
-let slideIndex = 1;
-
-function changeSlide(n) {
-  showSlide(slideIndex += n);
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 4000);
 }
 
-function showSlide(n) {
-  const slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
 }
 
-// Initially, you might want to start with the first slide displayed.
-showSlide(slideIndex);
-
-// Form Validation
-function validateForm() {
-  const name = document.forms["contactForm"]["Name"].value;
-  const email = document.forms["contactForm"]["Email"].value;
-  const message = document.forms["contactForm"]["Message"].value;
-
-  if (name === "" || email === "" || message === "") {
-    alert("All fields must be filled out");
-    return false;
-  }
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
-    $("#fadeInButton").hover(function () {
-      $("#animatedDiv").fadeIn();
-    });
+
+
+document.getElementById("colorBtn").addEventListener("click", function() {
+    var element = document.querySelector('.w3-content'); // Select the element with the class "example-class"
+    element.style.backgroundColor = "lightgrey";
+});
+
+document.getElementById("send").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevents the form from submitting and refreshing the page
+    var name = document.querySelector('input[name="Name"]').value;
+    var email = document.querySelector('input[name="Email"]').value;
+    var message = document.querySelector('input[name="Message"]').value;
+    if (name && email && message) {
+        alert("Form submitted successfully!\nName: " + name + "\nEmail: " + email + "\nMessage: " + message);
+    } else {
+        alert("Please fill out all fields!"); // Displays an alert if any field is empty
+    }
+});
+var colorChangeText = document.getElementById("colorChangeText");
+
+colorChangeText.addEventListener("mouseover", function() {
+    colorChangeText.style.color = "red"; // Change text color to red on hover
+});
+
+colorChangeText.addEventListener("mouseout", function() {
+    colorChangeText.style.color = "black"; // Change text color back to black when not hovering
+});
+
+
+var dropdownContent = document.getElementById("mydrop");
+
+var isDropdownVisible = false;
+
+function toggleDropdown() {
+    if (isDropdownVisible) {
+        dropdownContent.style.animation = "none";
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+        dropdownContent.style.animation = "slideDown 0.5s forwards";
+    }
+    isDropdownVisible = !isDropdownVisible;
+}
